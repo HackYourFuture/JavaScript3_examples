aef45b88d8fbe9ebb46116d4662567917ec71695
Jim Cramer <remarcmij@gmail.com>
2018-10-10 19:35:41 +0200

    Add Lauren's cooking pasta example

---
+++ src/week2/cooking-pasta/1-cooking-pasta-promise.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const PROCESSING_TIME_MS = 8000;
+
+function process(action, item) {
+  return new Promise(resolve => {
+    console.log(`${action} ${item}...`);
+    setTimeout(resolve, PROCESSING_TIME_MS);
+  });
+}
+
+function gatherIngredients() {
+  return process('gather', 'ingredients');
+}
+
+function cutTomatoes() {
+  return process('cut', 'tomatoes');
+}
+
+function cutGarlic() {
+  return process('cut', 'garlic');
+}
+
+function heatWater() {
+  return process('heat', 'water');
+}
+
+function cookPasta() {
+  return process('cook', 'pasta');
+}
+
+function fryGarlic() {
+  return process('fry', 'garlic');
+}
+
+function fryTomatoes() {
+  return process('fry', 'tomatoes');
+}
+
+function mixSauce() {
+  return process('mix', 'sauce');
+}
+
+function servePasta() {
+  return process('serve', 'pasta');
+}
+
+function startIntervalTimer() {
+  let elapsedSecs = 0;
+  return setInterval(() => {
+    elapsedSecs += 1;
+    console.log('...' + elapsedSecs);
+  }, 995);
+}
+
+function main() {
+  const timerId = startIntervalTimer();
+
+  console.log('Preparing @razpudding\'s pasta recipe...');
+
+  gatherIngredients()
+    .then(cutGarlic)
+    .then(cutTomatoes)
+    .then(() => Promise.all([fryGarlic(), fryTomatoes(), heatWater()]))
+    .then(() => Promise.all([mixSauce(), cookPasta()]))
+    .then(cookPasta)
+    .then(servePasta)
+    .then(() => {
+      clearInterval(timerId);
+      console.log('Enjoy this delicious pasta!');
+    });
+}
+
+main();
---
+++ src/week2/cooking-pasta/2-cooking-pasta-cb.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const PROCESSING_TIME_MS = 8000;
+
+function process(action, item, cb) {
+  console.log(`${action} ${item}...`);
+  setTimeout(cb, PROCESSING_TIME_MS);
+}
+
+function gatherIngredients(cb) {
+  process('gather', 'ingredients', cb);
+}
+
+function cutTomatoes(cb) {
+  process('cut', 'tomatoes', cb);
+}
+
+function cutGarlic(cb) {
+  process('cut', 'garlic', cb);
+}
+
+function heatWater(cb) {
+  process('heat', 'water', cb);
+}
+
+function cookPasta(cb) {
+  process('cook', 'pasta', cb);
+}
+
+function fryGarlic(cb) {
+  process('fry', 'garlic', cb);
+}
+
+function fryTomatoes(cb) {
+  process('fry', 'tomatoes', cb);
+}
+
+function mixSauce(cb) {
+  process('mix', 'sauce', cb);
+}
+
+function servePasta(cb) {
+  process('serve', 'pasta', cb);
+}
+
+function processAll(todos, cb) {
+  let todoCount = todos.length;
+
+  function processTodo(todoFn, cb) {
+    todoFn(() => {
+      todoCount -= 1;
+      if (todoCount === 0) {
+        cb();
+      }
+    });
+  }
+
+  todos.forEach(todo => processTodo(todo, cb));
+}
+
+function startIntervalTimer() {
+  let elapsedSecs = 0;
+  return setInterval(() => {
+    elapsedSecs += 1;
+    console.log('...' + elapsedSecs);
+  }, 995);
+}
+
+function main() {
+  const timerId = startIntervalTimer();
+
+  console.log('Preparing @razpudding\'s pasta recipe...');
+
+  gatherIngredients(() => {
+    cutGarlic(() => {
+      cutTomatoes(() => {
+        processAll([fryGarlic, fryTomatoes, heatWater], () => {
+          processAll([mixSauce, cookPasta], () => {
+            servePasta(() => {
+              clearInterval(timerId);
+              console.log('Enjoy this delicious pasta!');
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+main();
