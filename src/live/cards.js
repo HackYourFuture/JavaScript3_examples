8553404ec17fbda46e5791f3dce4a41779624844
Jim Cramer <remarcmij@gmail.com>
2018-10-27 18:51:15 +0200

    Added cards live-coding starter file

---
+++ src/live/cards.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const CARD_SUITES = [
+  { symbol: '♦️', name: 'tiles' },
+  { symbol: '♠️', name: 'pikes' },
+  { symbol: '♥️', name: 'hearts' },
+  { symbol: '♣️', name: 'clovers' },
+];
+
+const CARD_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
