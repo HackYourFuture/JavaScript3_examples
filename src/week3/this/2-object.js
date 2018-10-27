4ab2a7dc0acc7ecb28b80bbba6c159a8918615c1
Jim Cramer <remarcmij@gmail.com>
2018-10-27 23:50:42 +0200

    More on this, call, apply & bind

--- src/week3/this/2-object.js
+++ src/week3/this/2-object.js
@@ -13,7 +13,7 @@ const myDog = new Dog('Tarzan', 'brown');
 myDog.greet('Hi', '!');
 
 const greet = myDog.greet;
-greet('Hi', '!');
+// greet('Hi', '!');
 
 greet.call(myDog, 'Are you there,', ' ? ');
 
@@ -22,8 +22,24 @@ greet.apply(myDog, ['Nice dog,', '!']);
 const greet2 = myDog.greet.bind(myDog);
 greet2('Hi there again', '!');
 
+function greetDog(greeting, punctuation) {
+  console.log(greeting + ' ' + this.name + punctuation);
+}
+
 const herDog = {
-  name: 'Rocky'
+  name: 'Rocky',
+  greet: greetDog
 };
 
+herDog.greet('Where are you', '?');
+
 greet.call(herDog, 'How about you', '?');
+
+greetDog.call(herDog, 'Come here', '!');
+
+const greetHerDog = greetDog.bind(herDog);
+
+greetHerDog('Sit up,', '!');
+
+const greetHerDogHello = greetDog.bind(herDog, 'Hello');
+greetHerDogHello('!!!');
