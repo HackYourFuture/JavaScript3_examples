9329dbc3ca99b2c7f7219ee593619b11bfc010dc
Jim Cramer <remarcmij@gmail.com>
2018-10-28 09:36:04 +0100

    Add myCat

--- src/week3/this/3-object.js
+++ src/week3/this/3-object.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const myCat = {
+  name: 'Binky',
+  color: 'black',
+  greet: function (greeting, closing) {
+    console.log(greeting + ' ' + this.name + closing);
+  }
+};
+
+myCat.greet('Hello', ', where have you been?');
+
+
 function Dog(name, color) {
   this.name = name;
   this.color = color;
 
-  this.greet = function (greeting, punctuation) {
-    console.log(greeting + ' ' + this.name + punctuation);
+  this.greet = function (greeting, closing) {
+    console.log(greeting + ' ' + this.name + closing);
   };
 }
 
@@ -22,8 +33,8 @@ greet.apply(myDog, ['Nice dog,', '!']);
 const greet2 = myDog.greet.bind(myDog);
 greet2('Hi there again', '!');
 
-function greetDog(greeting, punctuation) {
-  console.log(greeting + ' ' + this.name + punctuation);
+function greetDog(greeting, closing) {
+  console.log(greeting + ' ' + this.name + closing);
 }
 
 const herDog = {
