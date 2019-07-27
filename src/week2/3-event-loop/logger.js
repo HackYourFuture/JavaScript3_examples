a455aebc8a03a7afe0c01c068f827dead7d37a43
Jim Cramer <remarcmij@gmail.com>
2019-07-27 09:45:57 +0200

    Refactor logger from ES6 class to plain object

--- src/week2/3-event-loop/logger.js
+++ src/week2/3-event-loop/logger.js
@@ -3,17 +3,13 @@
 {
   const INDENT_SPACES = 2;
 
-  class Logger {
-    constructor() {
-      this.level = 0;
-    }
-
+  window.logger = {
+    level: 0,
     enter(name) {
       const indentation = ' '.repeat(this.level * INDENT_SPACES);
       console.log(`${indentation}entering ${name}`);
       this.level += 1;
-    }
-
+    },
     leave(name) {
       this.level -= 1;
       const indentation = ' '.repeat(this.level * INDENT_SPACES);
@@ -21,8 +17,6 @@
       if (this.level === 0) {
         console.log('-'.repeat(40));
       }
-    }
-  }
-
-  window.logger = new Logger();
+    },
+  };
 }
